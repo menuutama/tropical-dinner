9ddf6c6bbc5574b59d6baff46e0c602e787e7d7e
menuutama <rani.yong203@gmail.com>
2026-06-08 17:23:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let isAdminMode = false;
 
 /* =========================
    INIT
-========================= */
+========================= */ 
 
 document.addEventListener("DOMContentLoaded", () => {
   setupSearch();
